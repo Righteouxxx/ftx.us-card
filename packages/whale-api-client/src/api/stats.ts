1028d05953aaf2ef654fcc9ab0fc1f8036e30543
Johnathon Locke <95666174+Righteouxxx@users.noreply.github.com>
2022-03-23 09:03:04 -0400

    Initialize monstrobishi/fix-masternode-test

---
+++ packages/whale-api-client/src/api/stats.ts
@@ -0,0 +1,47 @@
+import { WhaleApiClient } from '../whale.api.client'
+
+export class Stats {
+  constructor (private readonly client: WhaleApiClient) {
+  }
+
+  /**
+   * Get stats of DeFi Blockchain
+   *
+   * @return {Promise<StatsData>}
+   */
+  async get (): Promise<StatsData> {
+    return await this.client.requestData('GET', 'stats')
+  }
+}
+
+/**
+ * Stats data, doesn't use BigNumber is precision is not expected.
+ */
+export interface StatsData {
+  count: {
+    blocks: number
+    tokens: number
+    prices: number
+    masternodes: number
+  }
+  tvl: {
+    total: number
+    dex: number
+    masternodes: number
+  }
+  burned: {
+    total: number
+    fee: number
+    emission: number
+    address: number
+  }
+  price: {
+    usdt: number
+  }
+  masternodes: {
+    locked: Array<{weeks: number, tvl: number, count: number}>
+  }
+  blockchain: {
+    difficulty: number
+  }
+}
---
+++ src/module.indexer/_module.ts
@@ -0,0 +1,38 @@
+import { Module } from '@nestjs/common'
+import { RPCBlockProvider } from '@src/module.indexer/rpc.block.provider'
+import { MainIndexer } from '@src/module.indexer/model/_main'
+import { BlockIndexer } from '@src/module.indexer/model/block'
+import { ScriptActivityIndexer } from '@src/module.indexer/model/script.activity'
+import { ScriptAggregationIndexer } from '@src/module.indexer/model/script.aggregation'
+import { ScriptUnspentIndexer } from '@src/module.indexer/model/script.unspent'
+import { TransactionIndexer } from '@src/module.indexer/model/transaction'
+import { TransactionVinIndexer } from '@src/module.indexer/model/transaction.vin'
+import { TransactionVoutIndexer } from '@src/module.indexer/model/transaction.vout'
+import { VoutFinder } from '@src/module.indexer/model/_vout_finder'
+import { IndexStatusMapper } from '@src/module.indexer/status'
+import { DfTxIndexerModule } from '@src/module.indexer/model/dftx/_module'
+import { MainDfTxIndexer } from '@src/module.indexer/model/dftx.indexer'
+import { BlockMintedIndexer } from '@src/module.indexer/model/block.minted'
+
+@Module({
+  providers: [
+    RPCBlockProvider,
+    MainIndexer,
+    IndexStatusMapper,
+    VoutFinder,
+    BlockIndexer,
+    ScriptActivityIndexer,
+    ScriptAggregationIndexer,
+    ScriptUnspentIndexer,
+    TransactionIndexer,
+    TransactionVinIndexer,
+    TransactionVoutIndexer,
+    MainDfTxIndexer,
+    BlockMintedIndexer
+  ],
+  imports: [
+    DfTxIndexerModule
+  ]
+})
+export class IndexerModule {
+}
