840a0ceba3183fb600d3a7e3106b3d4d60c15995
Johnathon Locke <95666174+Righteouxxx@users.noreply.github.com>
2022-03-23 09:03:03 -0400

    Initialize canonbrother/mn-wallet

---
+++ packages/whale-api-client/__tests__/api/poolpairs.test.ts
@@ -0,0 +1,236 @@
+import { MasterNodeRegTestContainer } from '@defichain/testcontainers'
+import { StubWhaleApiClient } from '../stub.client'
+import { StubService } from '../stub.service'
+import { ApiPagedResponse, WhaleApiClient, WhaleApiException } from '../../src'
+import { addPoolLiquidity, createPoolPair, createToken, getNewAddress, mintTokens } from '@defichain/testing'
+import { PoolPairData } from '../../src/api/poolpairs'
+
+let container: MasterNodeRegTestContainer
+let service: StubService
+let client: WhaleApiClient
+
+beforeAll(async () => {
+  container = new MasterNodeRegTestContainer()
+  service = new StubService(container)
+  client = new StubWhaleApiClient(service)
+
+  await container.start()
+  await container.waitForWalletCoinbaseMaturity()
+  await service.start()
+
+  await setup()
+})
+
+afterAll(async () => {
+  try {
+    await service.stop()
+  } finally {
+    await container.stop()
+  }
+})
+
+async function setup (): Promise<void> {
+  const tokens = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
+
+  for (const token of tokens) {
+    await container.waitForWalletBalanceGTE(110)
+    await createToken(container, token)
+    await mintTokens(container, token)
+  }
+  await createPoolPair(container, 'A', 'DFI')
+  await createPoolPair(container, 'B', 'DFI')
+  await createPoolPair(container, 'C', 'DFI')
+  await createPoolPair(container, 'D', 'DFI')
+  await createPoolPair(container, 'E', 'DFI')
+  await createPoolPair(container, 'F', 'DFI')
+  await createPoolPair(container, 'G', 'DFI')
+  await createPoolPair(container, 'H', 'DFI')
+
+  await addPoolLiquidity(container, {
+    tokenA: 'A',
+    amountA: 100,
+    tokenB: 'DFI',
+    amountB: 200,
+    shareAddress: await getNewAddress(container)
+  })
+  await addPoolLiquidity(container, {
+    tokenA: 'B',
+    amountA: 50,
+    tokenB: 'DFI',
+    amountB: 300,
+    shareAddress: await getNewAddress(container)
+  })
+  await addPoolLiquidity(container, {
+    tokenA: 'C',
+    amountA: 90,
+    tokenB: 'DFI',
+    amountB: 360,
+    shareAddress: await getNewAddress(container)
+  })
+
+  // dexUsdtDfi setup
+  await createToken(container, 'USDT')
+  await createPoolPair(container, 'USDT', 'DFI')
+  await mintTokens(container, 'USDT')
+  await addPoolLiquidity(container, {
+    tokenA: 'USDT',
+    amountA: 1000,
+    tokenB: 'DFI',
+    amountB: 431.51288,
+    shareAddress: await getNewAddress(container)
+  })
+}
+
+describe('list', () => {
+  it('should list', async () => {
+    const response: ApiPagedResponse<PoolPairData> = await client.poolpairs.list(30)
+
+    expect(response.length).toStrictEqual(9)
+    expect(response.hasNext).toStrictEqual(false)
+
+    expect(response[1]).toStrictEqual({
+      id: '10',
+      symbol: 'B-DFI',
+      name: 'B-Default Defi token',
+      status: true,
+      tokenA: {
+        id: '2',
+        symbol: 'B',
+        reserve: '50',
+        blockCommission: '0'
+      },
+      tokenB: {
+        id: '0',
+        symbol: 'DFI',
+        reserve: '300',
+        blockCommission: '0'
+      },
+      apr: {
+        reward: 0,
+        total: 0
+      },
+      commission: '0',
+      totalLiquidity: {
+        token: '122.47448713',
+        usd: '1390.456752'
+      },
+      tradeEnabled: true,
+      ownerAddress: expect.any(String),
+      priceRatio: {
+        ab: '0.16666666',
+        ba: '6'
+      },
+      rewardPct: '0',
+      creation: {
+        tx: expect.any(String),
+        height: expect.any(Number)
+      }
+    })
+  })
+
+  it('should list with pagination', async () => {
+    const first = await client.poolpairs.list(4)
+    expect(first.length).toStrictEqual(4)
+    expect(first.hasNext).toStrictEqual(true)
+    expect(first.nextToken).toStrictEqual('12')
+
+    expect(first[0].symbol).toStrictEqual('A-DFI')
+    expect(first[1].symbol).toStrictEqual('B-DFI')
+    expect(first[2].symbol).toStrictEqual('C-DFI')
+    expect(first[3].symbol).toStrictEqual('D-DFI')
+
+    const next = await client.paginate(first)
+    expect(next.length).toStrictEqual(4)
+    expect(next.hasNext).toStrictEqual(true)
+    expect(next.nextToken).toStrictEqual('16')
+
+    expect(next[0].symbol).toStrictEqual('E-DFI')
+    expect(next[1].symbol).toStrictEqual('F-DFI')
+    expect(next[2].symbol).toStrictEqual('G-DFI')
+    expect(next[3].symbol).toStrictEqual('H-DFI')
+
+    const last = await client.paginate(next)
+    expect(last.length).toStrictEqual(1)
+    expect(last.hasNext).toStrictEqual(false)
+    expect(last.nextToken).toBeUndefined()
+
+    expect(last[0].symbol).toStrictEqual('USDT-DFI')
+  })
+})
+
+describe('get', () => {
+  it('should get', async () => {
+    const response: PoolPairData = await client.poolpairs.get('9')
+
+    expect(response).toStrictEqual({
+      id: '9',
+      symbol: 'A-DFI',
+      name: 'A-Default Defi token',
+      status: true,
+      tokenA: {
+        id: expect.any(String),
+        symbol: 'A',
+        reserve: '100',
+        blockCommission: '0'
+      },
+      tokenB: {
+        id: '0',
+        symbol: 'DFI',
+        reserve: '200',
+        blockCommission: '0'
+      },
+      apr: {
+        reward: 0,
+        total: 0
+      },
+      commission: '0',
+      totalLiquidity: {
+        token: '141.42135623',
+        usd: '926.971168'
+      },
+      tradeEnabled: true,
+      ownerAddress: expect.any(String),
+      priceRatio: {
+        ab: '0.5',
+        ba: '2'
+      },
+      rewardPct: '0',
+      creation: {
+        tx: expect.any(String),
+        height: expect.any(Number)
+      }
+    })
+  })
+
+  it('should throw error as numeric string is expected', async () => {
+    expect.assertions(2)
+    try {
+      await client.poolpairs.get('A-DFI')
+    } catch (err) {
+      expect(err).toBeInstanceOf(WhaleApiException)
+      expect(err.error).toStrictEqual({
+        code: 400,
+        type: 'BadRequest',
+        at: expect.any(Number),
+        message: 'Validation failed (numeric string is expected)',
+        url: '/v0.0/regtest/poolpairs/A-DFI'
+      })
+    }
+  })
+
+  it('should throw error while getting non-existent poolpair', async () => {
+    expect.assertions(2)
+    try {
+      await client.poolpairs.get('999')
+    } catch (err) {
+      expect(err).toBeInstanceOf(WhaleApiException)
+      expect(err.error).toStrictEqual({
+        code: 404,
+        type: 'NotFound',
+        at: expect.any(Number),
+        message: 'Unable to find poolpair',
+        url: '/v0.0/regtest/poolpairs/999'
+      })
+    }
+  })
+})
---
+++ src/module.api/_module.ts
@@ -0,0 +1,63 @@
+import { APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core'
+import { CacheModule, Module } from '@nestjs/common'
+import { RpcController } from '@src/module.api/rpc.controller'
+import { ActuatorController } from '@src/module.api/actuator.controller'
+import { TransactionController } from '@src/module.api/transaction.controller'
+import { ApiValidationPipe } from '@src/module.api/pipes/api.validation.pipe'
+import { AddressController } from '@src/module.api/address.controller'
+import { PoolPairController } from '@src/module.api/poolpair.controller'
+import { PoolPairService } from '@src/module.api/poolpair.service'
+import { DeFiDCache } from '@src/module.api/cache/defid.cache'
+import { SemaphoreCache } from '@src/module.api/cache/semaphore.cache'
+import { ExceptionInterceptor } from '@src/module.api/interceptors/exception.interceptor'
+import { ResponseInterceptor } from '@src/module.api/interceptors/response.interceptor'
+import { TokenController } from '@src/module.api/token.controller'
+import { BlockController } from '@src/module.api/block.controller'
+import { MasternodeController } from '@src/module.api/masternode.controller'
+import { ConfigService } from '@nestjs/config'
+import { NetworkName } from '@defichain/jellyfish-network'
+import { OracleController } from '@src/module.api/oracle.controller'
+import { PriceController } from '@src/module.api/price.controller'
+import { StatsController } from '@src/module.api/stats.controller'
+import { FeeController } from '@src/module.api/fee.controller'
+import { RawtxController } from '@src/module.api/rawtx.controller'
+
+/**
+ * Exposed ApiModule for public interfacing
+ */
+@Module({
+  imports: [CacheModule.register()],
+  controllers: [
+    RpcController,
+    AddressController,
+    ActuatorController,
+    TransactionController,
+    TokenController,
+    PoolPairController,
+    MasternodeController,
+    BlockController,
+    OracleController,
+    PriceController,
+    StatsController,
+    FeeController,
+    RawtxController
+  ],
+  providers: [
+    { provide: APP_PIPE, useClass: ApiValidationPipe },
+    // APP_INTERCEPTOR are only activated for /v* paths
+    { provide: APP_INTERCEPTOR, useClass: ResponseInterceptor },
+    { provide: APP_INTERCEPTOR, useClass: ExceptionInterceptor },
+    {
+      provide: 'NETWORK',
+      useFactory: (configService: ConfigService): NetworkName => {
+        return configService.get<string>('network') as NetworkName
+      },
+      inject: [ConfigService]
+    },
+    DeFiDCache,
+    SemaphoreCache,
+    PoolPairService
+  ]
+})
+export class ApiModule {
+}
