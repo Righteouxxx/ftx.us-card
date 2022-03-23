098f13de26803e2e220b6275b45f7ffce2eb6df9
Johnathon Locke <95666174+Righteouxxx@users.noreply.github.com>
2022-03-23 09:03:40 -0400

    Initialize canonbrother/support-reward-on-acc-history

---
+++ src/module.api/_core/api.query.ts
@@ -0,0 +1,29 @@
+import { IsBoolean, IsInt, IsOptional, IsString, Min } from 'class-validator'
+import { Transform, Type } from 'class-transformer'
+
+/**
+ * Pagination query with
+ * - size as limit/count/max query
+ * - next token for next slice/page
+ *
+ * This provides a singular consistent query pattern for DeFi Whale.
+ */
+export class PaginationQuery {
+  @IsOptional()
+  @IsInt()
+  @Transform(({ value }) => value > 200 ? 200 : value)
+  @Min(1)
+  @Type(() => Number)
+  size: number = 30
+
+  @IsOptional()
+  @IsString()
+  next?: string
+}
+
+export class AccountHistoryQuery extends PaginationQuery {
+  @IsOptional()
+  @IsBoolean()
+  @Transform(({ value }) => Boolean(value))
+  no_rewards?: boolean
+}
