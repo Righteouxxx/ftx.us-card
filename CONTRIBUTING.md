ddb996a88fb95a0f023258defdd9a154d8c71c40
Johnathon Locke <95666174+Righteouxxx@users.noreply.github.com>
2022-03-23 09:03:29 -0400

    Initialize canonbrother/indexing-create-loan-scheme

---
+++ CONTRIBUTING.md
@@ -0,0 +1,60 @@
+# DeFi Whale Contributing Guide
+
+## All features must be unit tested with accepted coverage. (Target 100%)
+
+Each package or functionality must be accompanied by full coverage testing.
+
+Due to Javascript type coercion, all test assertions must use strict equality checking.
+
+```diff
+-   expect(1).toBe('1')
+-   expect(1).toEqual('1')
++   expect(1).toStrictEqual(1)
+```
+
+## TODO comments
+
+TODO comments should usually include the author's github username in parentheses. Example:
+
+```ts
+// TODO(fuxingloh): Add tests.
+```
+
+## Code of conduct
+
+Please follow the guidelines outlined at https://github.com/DeFiCh/.github/blob/main/CODE_OF_CONDUCT.md
+
+## Explicit over implicit
+
+Each package, feature, code and decision should be explicit and well documented over implicitly guessing.
+
+## TypeScript
+
+TypeScript must be used for all code written in this project.
+
+> Heavily adapted from [deno style guide](https://github.com/denoland/deno/blob/main/docs/contributing/style_guide.md).
+
+### Minimize dependencies (target zero)
+
+### Do not depend on external code. (never if possible)
+
+### Use underscores or periods, not dashes in filenames.
+
+Example: Use `foo.bar.ts` instead of `foo-bar.ts`.
+
+### Exported functions: max 2 args, put the rest into an options object.
+
+### Use JSDoc for exported symbols.
+
+### Top level functions should not use arrow syntax.
+
+### `constants.ts` not allowed
+
+It's an anti-pattern for scaling code, it gives a false impression of separation of concern. All it does is create a
+mass of code concentration within project that were better separated.
+
+> An analogy for this problem is file organization in projects. Many of us have come to agree that organizing files by
+> file type (e.g. splitting everything into html, js and css folders) don't really scale. The code related to a feature
+> will be forced to be split between three folders, just for a false impression of "separation of concerns". The key
+> here is that "concerns" is not defined by file type. Instead, most of us opt to organize files by feature or
+> responsibility. https://github.com/vuejs/rfcs/issues/55#issuecomment-504875870
---
+++ src/module.indexer/model/dftx/set.loan.scheme.ts
@@ -0,0 +1,150 @@
+import { Injectable, Logger } from '@nestjs/common'
+import { SetLoanScheme, CSetLoanScheme } from '@defichain/jellyfish-transaction'
+import { LoanSchemeMapper } from '@src/module.model/loan.scheme'
+import { DeferredLoanSchemeMapper } from '@src/module.model/deferred.loan.scheme'
+import { LoanSchemeHistoryMapper, LoanSchemeHistoryEvent, LoanSchemeHistory } from '@src/module.model/loan.scheme.history'
+import { RawBlock } from '@src/module.indexer/model/_abstract'
+import { DfTxIndexer, DfTxTransaction } from '@src/module.indexer/model/dftx/_abstract'
+import { HexEncoder } from '@src/module.model/_hex.encoder'
+import { NotFoundIndexerError } from '@src/module.indexer/error'
+import BigNumber from 'bignumber.js'
+
+@Injectable()
+export class SetLoanSchemeIndexer extends DfTxIndexer<SetLoanScheme> {
+  OP_CODE: number = CSetLoanScheme.OP_CODE
+  private readonly logger = new Logger(SetLoanSchemeIndexer.name)
+
+  constructor (
+    private readonly loanSchemeMapper: LoanSchemeMapper,
+    private readonly loanSchemeHistoryMapper: LoanSchemeHistoryMapper,
+    private readonly deferredLoanSchemeMapper: DeferredLoanSchemeMapper
+  ) {
+    super()
+  }
+
+  async indexTransaction (block: RawBlock, transaction: DfTxTransaction<SetLoanScheme>, txIndex = 0): Promise<void> {
+    const data = transaction.dftx.data
+    const txid = transaction.txn.txid
+
+    const loanScheme = await this.loanSchemeMapper.get(data.identifier)
+    if (loanScheme !== undefined) {
+      return await this.update(block, data, txid, txIndex)
+    }
+
+    return await this.create(block, data, txid, txIndex)
+  }
+
+  private async create (block: RawBlock, data: SetLoanScheme, txid: string, txIndex: number): Promise<void> {
+    const loanScheme = {
+      id: data.identifier,
+      sort: `${HexEncoder.encodeHeight(block.height)}-${txIndex}-${txid}`,
+      minColRatio: data.ratio,
+      interestRate: data.rate.toString(),
+      activateAfterBlock: data.update.toString(),
+      block: {
+        hash: block.hash,
+        height: block.height,
+        medianTime: block.mediantime,
+        time: block.time
+      }
+    }
+
+    await this.loanSchemeMapper.put(loanScheme)
+
+    await this.loanSchemeHistoryMapper.put({
+      ...loanScheme,
+      id: `${data.identifier}-${txid}`,
+      loanSchemeId: data.identifier,
+      event: LoanSchemeHistoryEvent.CREATE
+    })
+  }
+
+  private async update (block: RawBlock, data: SetLoanScheme, txid: string, txIndex = 0): Promise<void> {
+    const loanScheme = {
+      id: data.identifier,
+      sort: `${HexEncoder.encodeHeight(block.height)}-${txIndex}-${txid}`,
+      minColRatio: data.ratio,
+      interestRate: data.rate.toString(),
+      activateAfterBlock: data.update.toString(),
+      block: {
+        hash: block.hash,
+        height: block.height,
+        medianTime: block.mediantime,
+        time: block.time
+      }
+    }
+
+    if (this.isActive(data, block.height)) {
+      await this.loanSchemeMapper.put(loanScheme)
+    } else {
+      await this.deferredLoanSchemeMapper.put({
+        ...loanScheme,
+        id: `${data.identifier}-${txid}`,
+        loanSchemeId: data.identifier,
+        activated: false
+      })
+    }
+
+    await this.loanSchemeHistoryMapper.put({
+      ...loanScheme,
+      id: `${data.identifier}-${txid}`,
+      loanSchemeId: data.identifier,
+      event: LoanSchemeHistoryEvent.UPDATE
+    })
+  }
+
+  async invalidateTransaction (block: RawBlock, transaction: DfTxTransaction<SetLoanScheme>, txIndex = 0): Promise<void> {
+    const data = transaction.dftx.data
+    const txid = transaction.txn.txid
+
+    if (this.isActive(data, block.height)) {
+      const previous = await this.getPrevious(data.identifier, block.height, txid, txIndex)
+      if (previous === undefined) {
+        throw new NotFoundIndexerError('index', 'LoanSchemeHistory', data.identifier)
+      }
+      await this.loanSchemeMapper.put({
+        id: previous.loanSchemeId,
+        sort: previous.sort,
+        minColRatio: previous.minColRatio,
+        interestRate: previous.interestRate,
+        activateAfterBlock: previous.activateAfterBlock,
+        block: previous.block
+      })
+    } else {
+      await this.deferredLoanSchemeMapper.delete(`${data.identifier}-${txid}`)
+    }
+
+    await this.loanSchemeHistoryMapper.delete(`${data.identifier}-${txid}`)
+  }
+
+  private isActive (loanScheme: SetLoanScheme, height: number): boolean {
+    return loanScheme.update.eq(0) ||
+           loanScheme.update.eq(new BigNumber('0xffffffffffffffff')) ||
+           new BigNumber(height).gte(loanScheme.update)
+  }
+
+  /**
+   * Get previous active loan scheme
+   */
+  private async getPrevious (id: string, height: number, txid: string, txIndex: number): Promise<LoanSchemeHistory | undefined> {
+    const findInNextPage = async (next: string, height: number): Promise<LoanSchemeHistory | undefined> => {
+      const list = await this.loanSchemeHistoryMapper.query(id, 100, next)
+      if (list.length === 0) {
+        return undefined
+      }
+
+      // get the closest activateAfterBlock against height
+      // ensure its queried by DESC height
+      // looking for the first height >= activateHeight
+      const prevActiveLoanScheme = list.find(each => new BigNumber(height).gte(new BigNumber(each.activateAfterBlock)))
+      if (prevActiveLoanScheme !== undefined) {
+        return prevActiveLoanScheme
+      }
+
+      const last = list[list.length - 1]
+      return await findInNextPage(last.sort, last.block.height)
+    }
+
+    return await findInNextPage(`${HexEncoder.encodeHeight(height)}-${txIndex}-${txid}`, height)
+  }
+}
