1a3d5ee677570dd75dfbad695aed3f8e4cf5a9d8
Johnathon Locke <95666174+Righteouxxx@users.noreply.github.com>
2022-03-23 09:03:55 -0400

    Initialize monstrobishi/poolswap-aggregate

---
+++ src/module.indexer/model/dftx/poolswap.ts
@@ -0,0 +1,88 @@
+import { DfTxIndexer, DfTxTransaction } from '@src/module.indexer/model/dftx/_abstract'
+import { PoolSwap, CPoolSwap } from '@defichain/jellyfish-transaction'
+import { RawBlock } from '@src/module.indexer/model/_abstract'
+import { Inject, Injectable } from '@nestjs/common'
+import { PoolPairMapper, PoolPair } from '@src/module.model/poolpair'
+import { PoolPairTokenMapper } from '@src/module.model/poolpair.token'
+import { NetworkName } from '@defichain/jellyfish-network'
+import { IndexerError } from '@src/module.indexer/error'
+import BigNumber from 'bignumber.js'
+import { PoolSwapMapper } from '@src/module.model/poolswap'
+import { HexEncoder } from '@src/module.model/_hex.encoder'
+import { PoolSwapAggregatedMapper } from '@src/module.model/poolswap.aggregated'
+import { AggregationIntervals } from './poolswap.interval'
+
+@Injectable()
+export class PoolSwapIndexer extends DfTxIndexer<PoolSwap> {
+  OP_CODE: number = CPoolSwap.OP_CODE
+
+  constructor (
+    private readonly poolPairMapper: PoolPairMapper,
+    private readonly poolPairTokenMapper: PoolPairTokenMapper,
+    private readonly poolSwapMapper: PoolSwapMapper,
+    private readonly aggregatedMapper: PoolSwapAggregatedMapper,
+    @Inject('NETWORK') protected readonly network: NetworkName
+  ) {
+    super()
+  }
+
+  async indexTransaction (block: RawBlock, transaction: DfTxTransaction<PoolSwap>): Promise<void> {
+    const data = transaction.dftx.data
+    const poolPairToken = await this.poolPairTokenMapper.queryForTokenPair(data.fromTokenId, data.toTokenId)
+    if (poolPairToken === undefined) {
+      throw new IndexerError(`Pool for pair ${data.fromTokenId}, ${data.toTokenId} not found`)
+    }
+
+    const poolPair = await this.poolPairMapper.getLatest(`${poolPairToken.poolPairId}`)
+    if (poolPair !== undefined) {
+      await this.indexSwap(poolPair, data.fromTokenId, data.fromAmount, block, transaction.txn.txid)
+    }
+  }
+
+  async indexSwap (poolPair: PoolPair, fromTokenId: number, fromAmount: BigNumber, block: RawBlock, txid: string): Promise<void> {
+    await this.poolSwapMapper.put({
+      id: `${poolPair.poolPairId}-${txid}`,
+      key: poolPair.poolPairId,
+      sort: HexEncoder.encodeHeight(block.height) + txid,
+      poolPairId: poolPair.poolPairId,
+      fromAmount: fromAmount.toFixed(8),
+      fromTokenId: fromTokenId,
+      block: {
+        hash: block.hash,
+        height: block.height,
+        time: block.time,
+        medianTime: block.mediantime
+      }
+    })
+
+    for (const interval of AggregationIntervals) {
+      const previous = await this.aggregatedMapper.query(`${poolPair.poolPairId}-${interval}`, 1)
+      const aggregate = previous[0]
+      aggregate.aggregated.amounts[`${fromTokenId}`] =
+          aggregate.aggregated.amounts[`${fromTokenId}`] === undefined
+            ? fromAmount.toFixed(8)
+            : fromAmount.plus(aggregate.aggregated.amounts[`${fromTokenId}`]).toFixed(8)
+      await this.aggregatedMapper.put(aggregate)
+    }
+  }
+
+  async invalidateSwap (poolPairId: string, txid: string): Promise<void> {
+    await this.poolSwapMapper.delete(`${poolPairId}-${txid}`)
+  }
+
+  async invalidateTransaction (block: RawBlock, transaction: DfTxTransaction<PoolSwap>): Promise<void> {
+    const data = transaction.dftx.data
+    const poolPairToken = await this.poolPairTokenMapper.queryForTokenPair(data.fromTokenId, data.toTokenId)
+
+    if (poolPairToken === undefined) {
+      throw new IndexerError(`Pool for pair ${data.fromTokenId}, ${data.toTokenId} not found`)
+    }
+
+    const poolPair = await this.poolPairMapper.getLatest(`${poolPairToken.poolPairId}`)
+    if (poolPair === undefined) {
+      throw new IndexerError(`Pool with id ${poolPairToken.poolPairId} not found`)
+    }
+
+    await this.invalidateSwap(poolPair.poolPairId, transaction.txn.txid)
+  }
+}
---
+++ src/module.model/poolpair.token.ts
@@ -0,0 +1,69 @@
+import { Model, ModelMapping } from '@src/module.database/model'
+import { Injectable } from '@nestjs/common'
+import { Database, SortOrder } from '@src/module.database/database'
+
+const PoolPairTokenMapping: ModelMapping<PoolPairToken> = {
+  type: 'poolpair_token',
+  index: {
+    token_key: {
+      name: 'poolpair_token_key_sort',
+      partition: {
+        type: 'string',
+        key: (b: PoolPairToken) => b.sort
+      }
+    }
+  }
+}
+
+@Injectable()
+export class PoolPairTokenMapper {
+  public constructor (protected readonly database: Database) {
+  }
+
+  async query (key: string, limit: number, lt?: string): Promise<PoolPairToken[]> {
+    return await this.database.query(PoolPairTokenMapping.index.token_key, {
+      partitionKey: key,
+      limit: limit,
+      order: SortOrder.DESC,
+      lt: lt
+    })
+  }
+
+  async queryForTokenPair (tokenA: number, tokenB: number, lt?: string): Promise<PoolPairToken | undefined> {
+    const result = await this.database.get(PoolPairTokenMapping, `${tokenA}-${tokenB}`)
+    if (result !== undefined) {
+      return result
+    }
+
+    return await this.database.get(PoolPairTokenMapping, `${tokenB}-${tokenA}`)
+  }
+
+  async list (limit: number, gt?: string): Promise<PoolPairToken[]> {
+    return await this.database.query(PoolPairTokenMapping.index.token_key, {
+      limit: limit,
+      order: SortOrder.ASC,
+      gt: gt
+    })
+  }
+
+  async put (pool: PoolPairToken): Promise<void> {
+    return await this.database.put(PoolPairTokenMapping, pool)
+  }
+
+  async delete (id: string): Promise<void> {
+    return await this.database.delete(PoolPairTokenMapping, id)
+  }
+}
+
+export interface PoolPairToken extends Model {
+  id: string // ---------| tokenA-tokenB
+  sort: string // -------| poolPairId (hex encoded)
+  poolPairId: number
+
+  block: {
+    hash: string
+    height: number
+    time: number
+    medianTime: number
+  }
+}
